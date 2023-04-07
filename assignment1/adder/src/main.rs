d27425f62f670afbac7a4051ed0df0e38dc95ff8
yatharthvyas <yatharth2000.vyas@gmail.com>
2023-04-07 14:32:58 -0700

    support for add, subtract and negate

--- assignment1/adder/src/main.rs
+++ assignment1/adder/src/main.rs
@@ -1,11 +1,40 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use sexp::*;
+use sexp::Atom::*;
 
-// Compile a source program into a string of x86-64 assembly
-fn compile(program: String) -> String {
-    let num = program.trim().parse::<i32>().unwrap();
-    return format!("mov rax, {}", num);
+enum Expr {
+    Num(i32),
+    Add1(Box<Expr>),
+    Sub1(Box<Expr>),
+    Negate(Box<Expr>)
+}
+
+// sexpr to expr
+fn parse_expr(s: &Sexp) -> Expr {
+    match s {
+        Sexp::Atom(I(n)) => Expr::Num(i32::try_from(*n).unwrap()),
+        Sexp::List(vec) => {
+            match &vec[..] {
+                [Sexp::Atom(S(op)), e] if op == "add1" => Expr::Add1(Box::new(parse_expr(e))),
+                [Sexp::Atom(S(op)), e] if op == "sub1" => Expr::Sub1(Box::new(parse_expr(e))),
+                [Sexp::Atom(S(op)), e] if op == "negate" => Expr::Negate(Box::new(parse_expr(e))),
+                _ => panic!("parse error"),
+            }
+        },
+        _ => panic!("parse error"),
+    }
+}
+
+// expr to assembly
+fn compile_expr(e: &Expr) -> String {
+    match e {
+        Expr::Num(n) => format!("mov rax, {}", *n),
+        Expr::Add1(subexpr) => compile_expr(subexpr) + "\nadd rax, 1",
+        Expr::Sub1(subexpr) => compile_expr(subexpr) + "\nsub rax, 1",
+        Expr::Negate(subexpr) => compile_expr(subexpr) + "\nneg rax",
+    }
 }
 
 fn main() -> std::io::Result<()> {
@@ -18,8 +47,8 @@ fn main() -> std::io::Result<()> {
     let mut in_contents = String::new();
     in_file.read_to_string(&mut in_contents)?;
 
-    let result = compile(in_contents);
-
+    let expr = parse_expr(&parse(&in_contents).unwrap());
+    let result = compile_expr(&expr);
     let asm_program = format!("
 section .text
 global our_code_starts_here
